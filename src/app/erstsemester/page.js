9ea6ebcb4e54263ebea6a2dc674c46a146771416
= <jonhur@web.de>
2024-10-19 11:35:00 +0200

    added timetable

--- src/app/erstsemester/page.js
+++ src/app/erstsemester/page.js
@@ -15,7 +15,11 @@ export default function() {
                 Wir haben für euch eine Ersti-Info erstellt. Diese könnt ihr euch <a href="/survival.pdf">hier</a> herunterladen.
             </div>
             <h2 className={styles.SmallHeading}>Stundenplan</h2>
-            <em >Beachtet auch die weiteren Hinweise unter dem Stundenplan: Es kommt noch mehr dazu!</em>
+            <em >
+                Beachtet auch die weiteren Hinweise unter dem Stundenplan: Es kommt noch mehr dazu!
+                <br/>
+                Keine Gewähr für Korrektheit!
+            </em>
             <Stundenplan />
             <div className={styles.Textblock}>
                 Zu den Mathe- und Informatik-Vorlesungen kommen noch <em className={styles.bold}>Übungstermine</em> hinzu.
@@ -69,17 +73,161 @@ export default function() {
 }
 
 function Stundenplan() {
+
+    const schedule = {
+        montag: {
+            8: null,
+            10: null,
+            12: {
+                title: "VL Analysis für Informatiker",
+                location: "M1",
+                address: "Einsteinstr. 64",
+                qisserverlink: "https://studium.uni-muenster.de/qisserver/rds?state=verpublish&status=init&vmfile=no&publishid=418177&moduleCall=webInfo&publishConfFile=webInfo&publishSubDir=veranstaltung",
+                learnweblink: "https://sso.uni-muenster.de/LearnWeb/learnweb2/enrol/index.php?id=79885"
+            },
+            14: {
+                title: "VL Informatik 1",
+                location: "M1",
+                address: "Einstreinst. 64",
+                qisserverlink: "",
+                learnweblink: ""
+            },
+            16: null,
+            18: {
+                title: "GIS-Grundkurs",
+                location: "GEO1, StudLab-125, 126, 130",
+                address: "Heisenbergstr. 2",
+                qisserverlink: "",
+                learnweblink: ""
+            }
+        },
+        dienstag: {
+            8: null,
+            10: {
+                title: "VL Einführung in die Geoinformatik",
+                location: "AP, HS",
+                address: "Corrensstr. 4",
+                qisserverlink: "",
+                learnweblink: ""
+            },
+            12: null,
+            14: null,
+            16: null,
+            18: null
+        },
+        mittwoch: {
+            8: null,
+            10: null,
+            12: null,
+            14: {
+                title: "Ü Mathe Ergänzungskurs",
+                location: "GEO1-255",
+                address: "Heisenbergstr. 2",
+                qisserverlink: "",
+                learnweblink: ""
+            },
+            16: null,
+            18: null
+        },
+        donnerstag: {
+            8: null,
+            10: null,
+            12: {
+                title: "VL Analysis für Informatiker",
+                location: "M1",
+                address: "Einsteinstr. 64",
+                qisserverlink: "",
+                learnweblink: ""
+            },
+            14: {
+                title: "VL Informatik 1",
+                location: "M1",
+                address: "Einsteinstr. 64",
+                qisserverlink: "",
+                learnweblink: ""
+            },
+            16: {
+                title: "Einführung in Java",
+                location: "M1",
+                address: "Einsteinstr. 64",
+                qisserverlink: "",
+                learnweblink: ""
+            },
+            18: null
+        },
+        freitag: {
+            8: null,
+            10: null,
+            12: null,
+            14: null,
+            16: null,
+            18: null
+        }
+    }
+
+    const tage = ["montag", "dienstag", "mittwoch", "donnerstag", "freitag"]
+    const times = []
+    for(var i = 8; i<=18; i+=2){
+        times.push(i)
+    }
+
     return(
         <>
-            <table>
+            <table className={styles.Table}>
                 <thead>
                     <tr>
                         <td>Zeit</td>
                         <td>Montag</td>
                         <td>Dienstag</td>
+                        <td>Mittwoch</td>
+                        <td>Donnerstag</td>
+                        <td>Freitag</td>
                     </tr>
                 </thead>
+                <tbody>
+                    {times.map(time => <Timeslot schedule={schedule} tage={tage} time={time}/> )}
+                </tbody>
             </table>
+        </>
+    )
+}
+
+const Timeslot = ({schedule, tage, time}) => {
+    return(
+        <tr>
+            <td>{time} - {time+2}</td>
+            {
+                tage.map(
+                    tag => {
+                        return(
+                            <td>
+                                {
+                                    schedule[tag][time] ?
+                                    <Timeslotentry
+                                        title={schedule[tag][time].title}
+                                        location = {schedule[tag][time].location}
+                                        address = {schedule[tag][time].address}
+                                        qisserverlink = {schedule[tag][time].qisserverlink}
+                                        learnweblink = {schedule[tag][time].learnweblink}
+                                    /> :
+                                    ""
+                                }
+                            </td>
+                        )
+                    }
+                )
+            }
+        </tr>
+    )
+}
+
+const Timeslotentry = ({title, location, address, qisserverlink, learnweblink}) => {
+    return(
+        <>
+            {qisserverlink ? <a href={qisserverlink}>{title}</a> : title}
+            {location ? <><br/>{location}</> : ""}
+            {address ? <><br/>({address})</> : ""}
+            {learnweblink ? <><br/><a href={learnweblink}>Learnweb</a></> : ""}
         </>
     )
 }
