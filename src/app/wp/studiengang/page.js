66a1790d5a526f2f703f1fa1b45ba23aa28866d3
= <jonhur@web.de>
2024-10-19 17:38:53 +0200

    added studiengang page

---
+++ src/app/wp/studiengang/page.js
@@ -0,0 +1,193 @@
+import styles from "./../../page.module.css"
+
+export default function () {
+    return (
+        <>
+            <h1 className={styles.BigHeading}>Der Studiengang Geoinformatik</h1>
+            <div className={styles.Textblock}>
+                Auf dieser Seite erfahrt ihr, welche Anforderungen das Studium der Geoinformatik an euch stellt.
+                Das Studium besteht aus mehreren Abschnitten, die einzelnen Module werden detailliert besprochen.
+                Nicht alle Module beinhalten Veranstaltungen am Institut für Geoinformatik.
+                Geoinformatik ist ein interdiszplinärer Studiengang und beinhaltet auch Vorlesungen und Seminare aus
+                den Fachbereichen Mathematik/Informatik und den Instituten der Geographie und Landschaftsökologie.
+            </div>
+
+            <h2 className={styles.SmallHeading}>Aufbau des Studiengangs</h2>
+            <div className={styles.Textblock}>
+                Der Bachelorstudiengang Geoinformatik ist, wie jeder andere Bachelorstudiengang, modular aufgebaut,
+                das heißt Lehrveranstaltungen werden in kleine, thematisch zusammengehörende Einheiten verpackt.
+                Ein einzelnes Modul schließt man in maximal 2 Semestern ab, wobei man mehrere Module parallel studiert.
+            </div>
+            <div className={styles.Textblock}>
+                Jedes Modul besteht aus mehreren Veranstaltungen; so besteht das Modul Informatik I aus den Veranstaltungen
+                Vorlesung und Übung Informatik 1 und einem Java-Programmierkurs. Für gewöhnlich wird über jede Vorlesung eine
+                Klausur geschrieben, in den Übungen muss man Übungsaufgaben bearbeiten, in denen eine gewisse Mindestpunktzahl
+                erreicht werden muss, um den Leistungsnachweis zu erhalten und zur Klausur zugelassen zu werden.
+            </div>
+            <div className={styles.Textblock}>
+                Weitere Prüfungsleistungen sind Hausarbeiten, Referate und Projekte. Jede Prüfungsleistung wird benotet und geht
+                mit in die Endnote ein. Am Ende des Bachelorstudiums steht die Bachelorarbeit an. Diese wird parallel zu den
+                Veranstaltungen in der Regel im 6. Semester geschrieben.
+            </div>
+            <div className={styles.Textblock}>
+                In einigen Studiengängen sind Betriebspraktika und Exkursionen Pflicht. Das ist in dem Bachelor Geoinformatik
+                nicht der Fall. Die Praktika und Exkursionen, die abgeleistet werden müssen, sind im Studienverlauf integriert
+                und werden vom Institut als Seminare angeboten. Es gibt aber die Möglichkeit sich Praktika im Bereich
+                „General Studies“ anrechnen zu lassen.
+            </div>
+            <div className={styles.Textblock}>
+                Seinen „Bachelor of Science Geoinformatik“ bekommt man mit dem Bestehen der Bachelorarbeit und dem Nachweis
+                von 180 Credit Points in seinem Studium, die man in den verschiedenen Modulen „verdient“. Ein CP soll dabei
+                30 Zeitstunden Arbeit widerspiegeln. Die Bachelorarbeit ist 12CP „wert“.
+            </div>
+
+            <h3 className={styles.VerySmallHeading}>
+                Wenn nun die Bachelorarbeit bestanden ist, die 180CP gesammelt sind und man den BSc Geoinformatik besitzt,
+                wie geht es dann weiter?
+            </h3>
+            <div className={styles.Textblock}>
+                Grundsätzlich ist der Bachelor of Science eine Art Zugangsberechtigung für einen Master of Science,
+                wie das Abitur eine Zugangsberechtigung zu einem Bachelorstudiengang ist.
+            </div>
+            <div className={styles.Textblock}>
+                Anders als bei der Hochschulzugangsberechtigung wird aber bei der Auswahl für den Master of Science nicht
+                auf den Numerus Clausus oder die Wartezeit geschaut, sondern darauf, was der Bewerber studiert hat.
+                So ist es schwierig, wenn sich ein Bachelor Biologie für den Master Geoinformatics bewirbt und ihm würde
+                wohl ein Absolvent Bachelor Geoinformatik vorgezogen werden. Anders sieht es aus wenn ein Bachelor
+                Geoinformatik sich für den Master Informatik bewirbt.
+            </div>
+            <div className={styles.Textblock}>
+                Was wir sagen möchten: man hat nach dem Bachelor noch immer eine gewisse Auswahlmöglichkeit darin,
+                was man machen möchte. Möchte man in dem Fachgebiet Geoinformatik bleiben, so kann man entweder am eigenen
+                Institut bleiben und sich für einen der beiden hier angebotenen Masterstudiengänge bewerben,
+                man kann an eine andere Universität gehen oder sich für ein verwandtes Fachgebiet bewerben.
+                Natürlich kann man mit dem Bachelor, der ja ein akademischer Abschluss ist, auch auf den Arbeitsmarkt gehen
+                und dort sein täglich Brot verdienen.
+            </div>
+
+            <h2 className={styles.SmallHeading}>Voraussetzugen - Studieninfos</h2>
+            <div className={styles.Textblock}>
+                Studienvoraussetzungen sind immer eine delikate Angelegenheit. Einerseits möchte man keinen vergraulen,
+                andererseits müssen manche Dinge einfach mal gesagt werden. Deshalb genießt diesen Text mit Vorsicht und
+                macht euch auch noch eigene Gedanken dazu. Am besten schaut ihr bei konkreten Fragen nochmal in die FAQ oder
+                schreibt uns eine Mail mit eurer Frage.
+            </div>
+            <h3 className={styles.VerySmallHeading}>
+                Abgedroschen, aber es muss sein: Zusammenarbeit.
+            </h3>
+            <div className={styles.Textblock}>
+                Das ist mal wieder das Allerwichtigste, denn wir haben gemerkt, dass es absolut notwendig ist,
+                um überhaupt Fuß zu fassen im Studium, dass ihr mit anderen im Team arbeiten könnt. Alleine seid ihr
+                aufgeschmissen, oder die absoluten Überflieger, die neben dem Studium wirklich gar nichts mehr machen.
+                Ihr bekommt so eine Fülle von Aufgabenzetteln (die zu lösen sind, meistens in Dreier-Gruppen), dass ihr,
+                wenn ihr jede Aufgabe alleine rechnen wollt, in arge Zeitnot geratet. Das ist wirklich kein Ammenmärchen,
+                das stimmt. Um euch die Sache ein wenig schmackhaft zu machen: Es macht auch durchaus Spaß zusammen zu arbeiten
+                und es bilden sich schnell gute Bekanntschaften, nicht nur mit einem kleinen Teil eurer Mitstudenten,
+                sondern mit fast allen. Das können wir aus eigener Erfahrung so sagen. Gerade etwas schüchternen Kollegen
+                und Kolleginnen fällt es so nicht so schwer, Kontakt zu Mitstudierenden zu finden.
+            </div>
+            <div className={styles.Textblock}>
+                Bitte bedenkt, dass einige Vorlesungen im späteren Verlauf des Bachelorstudiengangs auf Englisch gehalten werden.
+                Ihr solltet also ein gewisses Grundverständnis vorweisen und keine Angst mitbringen. Ansonsten besteht immernoch
+                die Möglichkeit während des Studiums Englischkurse an der Uni zu besuchen, für die ihr auch noch Credit Points
+                für das General Studies Modul sammeln könnt.
+            </div>
+
+            <h3 className={styles.VerySmallHeading}>
+                Analytisches und mathematisches Denkvermögen notwendig (v.a. im Grundstudium)
+            </h3>
+            <div className={styles.Textblock}>
+                Was weiterhin sehr wichtig ist (vorallendingen im rein mathematischen und informatischen Teil des Studiums)
+                und enorme Geschwindigkeitsvorteile bringt, ist die Fähigkeit, analytisch zu denken. Denn alles was aus der
+                Mathematik, wie auch mit der Informatik zu tun hat, ist pure Analytik. Wir schreiben absichtlich nicht
+                mathematisches Verständnis, oder Mathe-LK hier hin, weil es nicht zwangsläufig erforderlich ist.
+                Wer allerdings mit Mathe nie Probleme hatte, hat sicherlich einen Trumpf im Ärmel.
+                Denn wer die Zusammenhänge zwischen den Sachen erkennt, hat schon die halbe Miete gewonnen.
+                Es ist leider so, dass ohne Analytik nicht so viel geht. Wer aber auch ohne Mathe schnell ist, Zusammenhänge
+                zu erkennen, ist sehr willkommen. Denn der mathematische Stoff hat nicht mehr viel mit der Schulmathematik
+                zu tun. Alles wird abstrakter und übergreifender.
+            </div>
+
+            <h3 className={styles.VerySmallHeading}>
+                Na endlich: Geowissenschaften
+            </h3>
+
+            <div className={styles.Textblock}>
+                Hier braucht ihr euch keine Sorgen machen, wenn ihr kein Erdkunde in der Schule hattet. Das läßt sich schnell
+                verstehen und einholen. Was allerdings von Vorteil ist, ist das Interesse an geowissenschaftlichen Zusammenhängen,
+                die das Studium neben Mathe und Informatik eigentlich erst interessant machen und zu dem interdisziplinären Studiengang
+                machen, der er ist.<br />
+                Wenn ihr hieran kein oder nur wenig Interesse habt, dann seid ihr sicherlich besser im reinen Informatikstudiengang
+                aufgehoben. Allerdings muss man auch sagen, dass der Studiengang halt auch allein durch die Anwendung in den
+                Geowissenschaften sicherlich nicht so trocken rüberkommt, wie ein reiner Informatikstudiengang und auch eher
+                technisch orientiert ist.<br />
+                Weiterhin ist es durchaus möglich, sich aus der Geographie rauszuziehen, wenn man von drei Pflichtvorlesungen
+                absieht.
+            </div>
+
+            <h3 className={styles.VerySmallHeading}>
+                Und was muss ich an Computerkenntnissen mitbringen?
+            </h3>
+            <div>
+                Eigentlich gar keine speziellen Sachen. Was allerdings zwingend erforderlich ist, ist die Bereitschaft
+                sich mit dem Computer auseinanderzusetzen und selber Sachen auszuprobieren und nicht gleich verzweifeln,
+                wenn mal etwas nicht funktioniert, sondern dran bleiben und ausprobieren.
+            </div>
+
+            <h2 className={styles.SmallHeading}>Computa und andere "Legenden"</h2>
+            
+            <div className={styles.Textblock}>
+                Wie wir inzwischen bei mehreren Jahrgängen feststellten, ist der Bereich „Informatik“ in unserem Studiengang
+                gerade für Erstsemester ein schwierig zu kalkulierendes Gebiet. Alle Jahre wieder kommen zahlreiche Fragen,
+                die sich nur um dieses Thema, eventuell benötige Vorkenntnisse, Hardware, Software, Programmiererfahrung
+                und Ähnliches drehen.
+            </div>
+            <div className={styles.Textblock}>
+                Mit den folgenden Informationen möchten wir euch einen Teil dieser Fragen im Vorhinein abnehmen ;)
+            </div>
+            <h3 className={styles.VerySmallHeading}>Welche PC-Kenntnisse muss ich ins Studium mitbringen?</h3>
+            <div className={styles.Textblock}>
+                Du solltest nicht vollkommen ahnungslos vor einem PC sitzen und überlegen, wo man ihn anschaltet ;)
+                Ein Grundwissen im Umgang mit Computern sollte schon vorhanden sein – immerhin ist es auch ein informatischer
+                Studiengang und ganz ohne Computer geht das nun mal nicht. Allerdings wird von niemandem erwartet, das er
+                als Spezialist ins Studium geht. Ein gepflegtes Basis-Know-How reicht eigentlich vollkommen aus.
+            </div>
+            <h3 className={styles.VerySmallHeading}>Brauche ich ein Notebook, um das Studium aufnehmen zu können?</h3>
+            <div className={styles.Textblock}>
+                Nein, brauchst du nicht. Natürlich solltest du in diesem Fall einen Rechner zu Hause stehen haben.
+                In der Uni kannst du eines der vielen CIP-Pools nutzen. Jedoch wirst du schnell merken, dass es durchaus
+                seine Vorteile hat, jederzeit seine E-Mails abrufen zu können und seine eigene Arbeitsumgebung dabei zu haben um
+                beispielsweise Übungszettel bearbeiten zu können.
+            </div>
+            <h3 className={styles.VerySmallHeading}>Was für einen Rechner soll ich mitbringen?</h3>
+            <div className={styles.Textblock}>
+                Einen funktionstüchtigen ;) Die Hardwareanforderungen unseres Studiums sind nicht so maßlos hoch,
+                dass Du jetzt direkt in den nächsten großen Elektromarkt rennen und dort das neueste X-Ghz Monster kaufen mußt.
+                Was die Software angeht, die Du hinterher einsetzen wirst – hier empfiehlt es sich, entweder ein Microsoft
+                Windows Betriebssystem zu installieren oder in Linux so fit zu sein, das Du Windows-Software mittels entsprechender
+                Emulatoren zum Laufen bringen kannst. Leider ist es so, das der Großteil spezieller Geosoftware für
+                Windows-Plattformen konzipiert ist.
+            </div>
+            <h3 className={styles.VerySmallHeading}>Muss ich zu Studienbeginn schon programmieren können? Wenn ja, welche Sprachen?</h3>
+            <div className={styles.Textblock}>
+                Nein, mußt Du nicht. Wenn wir zu Studienbeginn alle schon programmieren könnten, bräuchten wir Informatik
+                wohl kaum noch studieren, oder?
+            </div>
+            <div className={styles.Textblock}>
+                Ernsthaft: Es ist keine Voraussetzung, dass man vor Studienbeginn schon mal programmiert hat.
+                Die meisten fangen diesen Studiengang ohne nennenswerte Programmierkenntnisse an und kommen trotzdem zurecht.
+                Bei den Informatikern sieht es da schon mal etwas anders aus, deshalb gibt es auch Profs, die zwar bei den Grundlagen
+                anfangen, aber relativ schnell vorgehen. In den letzten Jahren hat sich gezeigt, dass Programmierkenntnisse deshlab
+                den Einstieg ins Studium erleichtern.  Wenn Du im vorhinein Zeit hast und gerne schon mal ein bisschen vorarbeiten
+                willst, dann schau dir Java an, das kann in den ersten Vorlesungen durchaus hilfreich sein.
+                Java ist die bei uns am häufigsten eingesetzte Programmiersprache und wird immer mal wieder benötigt.
+            </div>
+            <h3 className={styles.VerySmallHeading}>Wieviel Geld muß ich in etwa pro Semester für Softwarekäufe einkalkulieren?</h3>
+            <div className={styles.Textblock}>
+                Pauschal keins. Die meiste Software, die wir benötigen, ist frei verfügbar und kann kostenlos heruntergeladen werden.
+                Für die wenigen Produkte, die nicht unter diese Kategorie fallen, gibt es zumeist Campus-Lizenzen – soll heißen,
+                Studenten zahlen nichts oder nur einen symbolischen Beitrag. Schaue hierfür mal bei den Software-Links vorbei.
+            </div>
+        </>
+    )
+}
