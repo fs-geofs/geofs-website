1d6123294201ec031e7afc2329de7ef06c8fd0e1
= <jonhur@web.de>
2024-10-19 10:32:09 +0200

    added Textblock

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,9 @@ export default function Home() {
     <>
       <h1 className={styles.BigHeading}>Herzlich Willkommen...</h1>
       <div className={styles.Textblock}>
-      …auf den Seiten der Fachschaft Geoinformatik an der Westfälischen Wilhelms-Universität Münster.
-      Auf den folgenden Seiten findet ihr Informationen zum Studium, der Fachschaft und vielem mehr,
-      was für euch als (zukünftige) Studierende der Geoinformatik interessant sein könnte.
+        …auf den Seiten der Fachschaft Geoinformatik an der Westfälischen Wilhelms-Universität Münster.
+        Auf den folgenden Seiten findet ihr Informationen zum Studium, der Fachschaft und vielem mehr,
+        was für euch als (zukünftige) Studierende der Geoinformatik interessant sein könnte.
       </div>
       <Termine />
       <Praesidienste />
@@ -84,17 +84,19 @@ function Praesidienste() {
     <>
       <h2 className={styles.SmallHeading}>Präsenzzeiten im Fachschaftsraum</h2>
       <div className={styles.Textblock}>
-        Während der Vorlesungszeit sind zu diesen Zeiten Fachschaftler und Fachschaftlerinnen im Raum anzutreffen,
-        die euch gerne Fragen beantworten. In den Semesterferien sind wir nach Vereinbarung vor Ort und stets per Mail
-        und Facebook/Instagram erreichbar.
-        <br/>
-        <br/>
-        Unseren Raum 024 findet ihr im Erdgeschoss des GEO1 – wenn man rein kommt auf der rechten Seite, direkt gegenüber vom Hörsaal!
-        Die Sitzungen finden in Präsenz statt. Im Normalfall finden diese in Raum 242 des GEO1 statt, also im Seminarraum im 2. Stock.
-        Kommt also gerne auch zur Sitzung vorbei, alternativ könnt ihr euch auf Discord dazu schalten,
-        dazu müsstet ihr euch einmal rechtzeitig vor der Sitzung bei uns per Mail melden.
-        In den Semesterferien finden die Sitzungen nur unregelmäßig statt, schreibt uns im Zweifelsfall vorher eine Mail,
-        damit wir euch informieren können, wann die nächste Sitzung stattfindet.
+          <div className={styles.Textblock}>
+          Während der Vorlesungszeit sind zu diesen Zeiten Fachschaftler und Fachschaftlerinnen im Raum anzutreffen,
+          die euch gerne Fragen beantworten. In den Semesterferien sind wir nach Vereinbarung vor Ort und stets per Mail
+          und Facebook/Instagram erreichbar.
+        </div>
+        <div className={styles.Textblock}>
+          Unseren Raum 024 findet ihr im Erdgeschoss des GEO1 – wenn man rein kommt auf der rechten Seite, direkt gegenüber vom Hörsaal!
+          Die Sitzungen finden in Präsenz statt. Im Normalfall finden diese in Raum 242 des GEO1 statt, also im Seminarraum im 2. Stock.
+          Kommt also gerne auch zur Sitzung vorbei, alternativ könnt ihr euch auf Discord dazu schalten,
+          dazu müsstet ihr euch einmal rechtzeitig vor der Sitzung bei uns per Mail melden.
+          In den Semesterferien finden die Sitzungen nur unregelmäßig statt, schreibt uns im Zweifelsfall vorher eine Mail,
+          damit wir euch informieren können, wann die nächste Sitzung stattfindet.
+        </div>
       </div>
       <table className={styles.Table}>
         <thead>
