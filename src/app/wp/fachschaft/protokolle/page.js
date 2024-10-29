161624ab9fe5996d5ce472021001a4e233ce8b88
= <jonhur@web.de>
2024-10-29 23:13:53 +0100

    added boilerplate pages

--- src/app/wp/fachschaft/protokolle/page.js
+++ src/app/wp/fachschaft/protokolle/page.js
@@ -3,7 +3,7 @@ import styles from "@/app/page.module.css"
 export default function () {
     return (
         <>
-            <h1 className={styles.BigHeading}>Kontakt</h1>
+            <h1 className={styles.BigHeading}>Protokolle</h1>
         </>
     )
 }
