a2ec31bdd95c8abef4ff75623454e29121720602
= <jonhur@web.de>
2024-10-29 23:21:49 +0100

    added boilerplate

---
+++ src/app/wp/english/page.js
@@ -0,0 +1,9 @@
+import styles from "@/app/page.module.css"
+
+export default function () {
+    return(
+        <>
+            <h1 className={styles.BigHeading}>English</h1>
+        </>
+    )
+}
