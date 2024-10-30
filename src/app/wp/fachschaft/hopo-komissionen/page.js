04c59df81d12a20e1140230d6e9cd408f63ef529
= <jonhur@web.de>
2024-10-30 18:54:24 +0100

    added hopo

--- src/app/wp/fachschaft/hopo-komissionen/page.js
+++ src/app/wp/fachschaft/hopo-komissionen/page.js
@@ -1,9 +1,81 @@
 import styles from "@/app/page.module.css"
 
 export default function () {
+
     return (
         <>
-            <h1 className={styles.BigHeading}>HoPo / Komissionen</h1>
+            <h1 className={styles.BigHeading}>Hochschulpolitik und Komissionen</h1>
+            <h2 className={styles.SmallHeading}>An unserem Fachbereich</h2>
+            <h3 className={styles.VerySmallHeading}>Die Fachschaft (FS)</h3>
+            <div className={styles.Textblock}>
+                „Das ist doch der Raum, wo es Altklausuren gibt, und die Leute, die die Geoparty organisieren.“ – An sich nicht
+                verkehrt, aber aus Sicht der Hochschulpolitik versteht man unter einer Fachschaft die Gesamtheit aller Studierenden, die sich für
+                ein Studienfach eingeschrieben haben. Das, was man im alltäglichen Sprachgebrauch als „Fachschaft“ bezeichnet, ist der Fachschaftsrat
+                (siehe unten). Das Hauptfach  legt fest, zu welcher Fachschaft ihr gehört. Damit ist auch festgelegt, welche Fachschaftsvertretung ihr
+                im Wintersemester wählen könnt.
+            </div>
+            <h3 className={styles.VerySmallHeading}>Der Fachschaftsrat (FSR)</h3>
+            <div className={styles.Textblock}>
+                Ein Beispiel für einen Fachschaftsrat sind wir – die Fachschaft Geoinformatik. Der Fachschaftsrat besteht aus all denjenigen, die die
+                Fachschaftsarbeit gestalten und für ihre Umsetzung verantwortlich sind. Nach außen hin nimmt man uns häufig als Organisatoren diverser
+                Veranstaltungen wie der O-Woche und der Geoparty wahr, aber auch hinter den Kulissen, in verschiedensten Gremien, leisten wir vieles für
+                die Studierenden unseres Fachbereichs. Falls ihr Lust habt, einen Einblick in unsere Arbeit zu bekommen, schaut doch mal bei einer unserer
+                wöchentlichen Fachschaftssitzungen vorbei. Die Sitzungen sind öffentlich, sodass ihr nach Lust und Laune vorbeischauen könnt. Den aktuellen
+                Termin findet ihr auf unserer Homepage.
+            </div>
+            <h3 className={styles.VerySmallHeading}>Die Fachschaftsvertretung (FSV)</h3>
+            <div className={styles.Textblock}>
+                Die Fachschaftsvertretung wird von allen Studierenden einer Fachschaft jedes Wintersemester gewählt. Bei uns besteht die FSV aus 9
+                Personen, die wiederum den Fachschaftsrat wählen – also letztendlich entscheiden, wer aus hochschulpolitischer Sicht zur Fachschaft
+                Geoinformatik gehört. Eine weitere Aufgabe der FSV ist die Kontrolle des Fachschaftsrates. Sie kann also als eine Art „Aufsichtsrat“
+                verstanden werden.
+            </div>
+            <h3 className={styles.VerySmallHeading}>Der Fachbereichsrat (FBR)</h3>
+            <div className={styles.Textblock}>
+                Das ist das oberste Gremium des Fachbereichs. Hier wird über alle Belange entschieden, die die Studierende und Mitarbeiter des
+                Fachbereichs betreffen. Unter anderem werden Änderungen an den Studien- und Prüfungsordnungen erlassen und der Jahresetat den
+                einzelnen Instituten des Fachbereichs zugewiesen. Außerdem wählt der FBR den Dekan, den Vorsteher des Fachbereichs. Der FBR besteht
+                aus Vertretern folgender vier Gruppierungen: Professoren, Studierende, wissenschaftliche Mitarbeiter und nichtwissenschaftliche Mitarbeiter.
+                Jede Gruppierung wählt im Sommersemester per Briefwahl ihre Vertreter.
+            </div>
+
+            <h2 className={styles.SmallHeading}>... und auf Universitätsebene</h2>
+            <h3 className={styles.VerySmallHeading}>Das Studierendenparlament</h3>
+            <div className={styles.Textblock}>
+                Das StuPa wird im Wintersemester zusammen mit den Fachschaftsvertretungen von allen Studierenden unserer Universität gewählt. Die Kandidaten
+                stellen sich in parteiähnlichen Gruppen, den so genannten Listen, zur Wahl. Diese sind recht zahlreich vorhanden und machen mehr oder weniger
+                in den Wahlwochen auf sich aufmerksam. Der AStA und das StuPa sind die einzigen vom Gesetz vorgeschriebenen Studentenvertretungen. Weitere
+                Informationen zur Arbeit des StuPa findet ihr auf dessen Homepage:<br/>
+                <a href="https://www.stupa.ms">https://www.stupa.ms</a>
+            </div>
+            <h3 className={styles.VerySmallHeading}>Der AStA</h3>
+            <div className={styles.Textblock}>
+                Das ist der Allgemeine Studierendenausschuss, die oberste Vertretung aller Studierenden der Uni Münster. Der AStA vertritt die Interessen
+                der Studierenden nach außen, z.B. gegenüber der Landesregierung. Gewählt wird der AStA durch das Studierendenparlament. Man kann den AStA
+                auch als eine Art Regierung auffassen, die die Beschlüsse des StuPa umsetzt. Dazu werden Referate gebildet, die für unterschiedliche
+                Bereiche und Belange zuständig sind. Darüber hinaus bietet der AStA einige Service-Leistungen an, z.B. kostenlose Rechtberatung und den
+                Bulli-Verleih. Das Büro des AStAs findet ihr in dem kleinen Häuschen links vorm Schloss. Weitere Infos zur Arbeit des AStA und der einzelnen
+                Referate könnt ihr auf der AStA-Homepage bekommen:<br/>
+                <a href="https://www.asta.ms">https://www.asta.ms</a>
+            </div>
+            <h3 className={styles.VerySmallHeading}>Der Senat</h3>
+            <div className={styles.Textblock}>
+                Der Senat wird zusammen mit den Fachbereichsräten im Sommer von allen Hochschullehrern, Studierenden, akademischen und nichtakademischen
+                Mitarbeitern der Universität gewählt. Die Aufgaben des Senats sind durch das Hochschulgesetz festgelegt und umfassen unter anderem die
+                Bestätigung der Wahl der Rektoratsmitglieder (oder die Empfehlung der Abwahl), die Änderung der Verfassung und Rahmenordnungen und die
+                Mitarbeit an der strategischen Planung der Universität. Außerdem kann der Senat im Rahmen seiner Zuständigkeiten Kommissionen und Ausschüsse
+                mit inhaltlich und zeitlich begrenzten Aufgaben bilden. Weitere Informationen dazu findet ihr hier:<br/>
+                <a href="https://www.uni-muenster.de/Senat">https://www.uni-muenster.de/Senat</a>
+            </div>
+            <h3 className={styles.VerySmallHeading}>Die Fachschaftenkonferenz (FK)</h3>
+            <div className={styles.Textblock}>
+                Die FK besteht aus einzelnen Vertretern sämtlicher Fachschaftsräte der Universität. Auf der FK werden Dinge aus dem Alltag der Fachschaften
+                im Speziellen und zur Hochschulpolitik im Allgemeinen thematisiert. Die FK dient als Plattform, um fachschaftsübergreifende Aktionen zu planen,
+                abzusprechen und durchzuführen. Außerdem verfügt die FK über einen finanziellen Sondertopf, dessen Gelder für Fachschaften und studentische
+                Initiativen zur Verfügung stehen, die ihre Ausgaben nicht komplett aus eigenen Mitteln refinanzieren können. Den Vorsitz bei der FK hat das
+                autonome Fachschaftenreferat inne.<br/>
+                <a href="https://www.asta.ms/index.php/fachschaftenkonferenz/">https://www.asta.ms/index.php/fachschaftenkonferenz/</a>
+            </div>
         </>
     )
 }
