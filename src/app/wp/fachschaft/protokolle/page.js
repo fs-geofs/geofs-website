70b8f5be77207ace108dd42525db81a40f5a18a7
= <jonhur@web.de>
2024-10-30 18:34:36 +0100

    added protokolle mock page

--- src/app/wp/fachschaft/protokolle/page.js
+++ src/app/wp/fachschaft/protokolle/page.js
@@ -4,6 +4,12 @@ export default function () {
     return (
         <>
             <h1 className={styles.BigHeading}>Protokolle</h1>
+            <div className={styles.Textblock}>
+                Die Protokolle zu den wöchentlichen Sitzungen findest du hier:
+            </div>
+            <div className={styles.Textblock} style={{border: "3px solid red", padding: "5px"}}>
+                <strong>Under Construction!</strong>
+            </div>
         </>
     )
 }
