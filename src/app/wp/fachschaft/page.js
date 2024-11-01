c7c26e0a99ee4ed38029cf5ea88c593fe9d36dcb
= <jonhur@web.de>
2024-11-01 16:18:49 +0100

    added pages for redirect

---
+++ src/app/wp/fachschaft/page.js
@@ -0,0 +1,11 @@
+import {redirect} from 'next/navigation'
+
+/*
+Diese Seite hat keine Content und soll nicht verlinkt werden.
+Autamatisches Reirect zur ersten Child-Seite (aus der Navbar)
+falls irgendein findiger GI diese URL ansteuert.
+*/
+
+export default function () {
+    redirect("/wp/fachschaft/das-sind-wir")
+}
