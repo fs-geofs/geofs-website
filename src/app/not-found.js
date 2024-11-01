d86696a6b7c7fc4287ca0747185a9943fab92819
= <jonhur@web.de>
2024-11-01 16:33:41 +0100

    added 404 page

---
+++ src/app/not-found.js
@@ -0,0 +1,16 @@
+import styles from "@/app/page.module.css"
+
+/*
+Diese Seite wird angezeigt bei einem 404 Error,
+also wenn eine ungültige URL eingegeben wird
+*/
+
+export default function(){
+    return (
+        <>
+            <h1 className={styles.BigHeading}>Error 404</h1>
+            <h2 className={styles.SmallHeading}>Seite nicht gefunden</h2>
+            <h2 className={styles.SmallHeading}>Page not Found</h2>
+        </>
+    )
+}
