929d70da384035b8b7d129e96156a52ff298ff88
= <jonhur@web.de>
2024-11-02 23:31:20 +0100

    added news page

--- src/app/wp/aktuelles/news/page.js
+++ src/app/wp/aktuelles/news/page.js
@@ -1,9 +1,32 @@
 import styles from "@/app/page.module.css"
 
-export default function () {
-    return(
+export default async function Site() {
+    
+    const blog_entries = []
+
+    // call API
+    const resp = await fetch(
+        "http://localhost:3000/api/news",
+        {method: "GET", cache: "no-store"}
+    )
+    
+    if (resp.status !== 200){
+        blog_entries.push("Error")
+    } else {
+        const entries = await resp.json()
+        entries.map(entry => {
+            const html_to_insert = entry.content
+                .replaceAll("<h1>", `<h1 class=${styles.BigHeading}>`)  // apply h1 formatting
+                .replaceAll("<h2>", `<h1 class=${styles.SmallHeading}>`) // apply h2 formatting
+                .replaceAll("<h3>", `<h1 class=${styles.VerySmallHeading}>`) //apply h3 formatting
+                .replaceAll("<div>", `<div class=${styles.Textblock}>`) // apply Textbox formatting to divs
+            blog_entries.push(<div dangerouslySetInnerHTML={{__html: html_to_insert}} key={entry.id}/>)}
+        )
+    }
+
+    return (
         <>
-            <h1 className={styles.BigHeading}>News</h1>
+            {blog_entries.map(entry => entry)}
         </>
     )
 }
