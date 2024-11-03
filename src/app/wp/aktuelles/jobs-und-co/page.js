d6f12acd4365e46f5d5dcf7b07fcf5a1166b86b2
= <jonhur@web.de>
2024-11-03 22:56:19 +0100

    added error handling

--- src/app/wp/aktuelles/jobs-und-co/page.js
+++ src/app/wp/aktuelles/jobs-und-co/page.js
@@ -2,6 +2,7 @@ import styles from "@/app/page.module.css"
 import localStyle from "./page.module.css"
 
 import { BACKEND_BASE } from "@/app/BACKEND_URL"
+import FetchError from "@/components/error_fetching"
 
 import DOMPurify from "isomorphic-dompurify"
 
@@ -10,30 +11,36 @@ export default async function Site() {
     const job_entries = []
 
     // call API to fetch news articles
-    const resp = await fetch(
-        `${BACKEND_BASE}/joblistings`,
-        { method: "GET", cache: "no-store" }
-    )
-
-    if (resp.status !== 200) {
-        job_entries.push("Error fetching Job listings!")
-    } else {
-        const entries = await resp.json()
-        entries.map(entry => {
-            const html_to_insert = DOMPurify.sanitize(
-                entry.content
-                    .replaceAll("<h1>", `<h1 class=${styles.BigHeading}>`)  // apply h1 formatting
-                    .replaceAll("<h2>", `<h1 class=${styles.SmallHeading}>`) // apply h2 formatting
-                    .replaceAll("<h3>", `<h1 class=${styles.VerySmallHeading}>`) //apply h3 formatting
-                    .replaceAll("<div>", `<div class=${styles.Textblock}>`) // apply Textbox formatting to divs
-            )
-            job_entries.push(
-                <div className={localStyle.Blogpost} key={entry.id}>
-                    <div dangerouslySetInnerHTML={{ __html: html_to_insert }} />
-                </div>
-            )
-        })
+    try {
+        const resp = await fetch(`${BACKEND_BASE}/joblistings`, { method: "GET", cache: "no-store" })
+        if (resp.status !== 200) {
+            throw new Error()
+        } else {
+            const entries = await resp.json()
+            entries.map(entry => {
+                const html_to_insert = DOMPurify.sanitize(
+                    entry.content
+                        .replaceAll("<h1>", `<h1 class=${styles.BigHeading}>`)  // apply h1 formatting
+                        .replaceAll("<h2>", `<h1 class=${styles.SmallHeading}>`) // apply h2 formatting
+                        .replaceAll("<h3>", `<h1 class=${styles.VerySmallHeading}>`) //apply h3 formatting
+                        .replaceAll("<div>", `<div class=${styles.Textblock}>`) // apply Textbox formatting to divs
+                )
+                job_entries.push(
+                    <div className={localStyle.Blogpost} key={entry.id}>
+                        <div dangerouslySetInnerHTML={{ __html: html_to_insert }} />
+                    </div>
+                )
+            })
+        }
+    } catch {
+        return (
+            <>
+            <h1 className={styles.BigHeading}>Jobs und Co</h1>
+            <FetchError />
+        </>
+        )
     }
+
 
 
     return (
--- src/app/wp/aktuelles/news/page.js
+++ src/app/wp/aktuelles/news/page.js
@@ -2,6 +2,7 @@ import styles from "@/app/page.module.css"
 import localStyle from "./page.module.css"
 
 import DOMPurify from "isomorphic-dompurify"
+import FetchError from "@/components/error_fetching"
 
 import { BACKEND_BASE } from "@/app/BACKEND_URL"
 
@@ -10,36 +11,41 @@ export default async function Site() {
     const blog_entries = []
 
     // call API to fetch news articles
-    const resp = await fetch(
-        `${BACKEND_BASE}/news`,
-        { method: "GET", cache: "no-store" }
-    )
-
-    if (resp.status !== 200) {
-        blog_entries.push("Error fetching news articles!")
-    } else {
-        const entries = await resp.json()
-        entries.map(entry => {
-            const html_to_insert = DOMPurify.sanitize(
-                entry.content
-                    .replaceAll("<h1>", `<h1 class=${styles.BigHeading}>`)  // apply h1 formatting
-                    .replaceAll("<h2>", `<h1 class=${styles.SmallHeading}>`) // apply h2 formatting
-                    .replaceAll("<h3>", `<h1 class=${styles.VerySmallHeading}>`) //apply h3 formatting
-                    .replaceAll("<div>", `<div class=${styles.Textblock}>`) // apply Textbox formatting to divs
-            )
-            blog_entries.push(
-                <div className={localStyle.Blogpost} key={entry.id}>
-                    <h4>{
-                        // reformat date string
-                        entry.date
-                            .split("-")
-                            .reverse()
-                            .join(".")
-                    }</h4>
-                    <div dangerouslySetInnerHTML={{ __html: html_to_insert }} />
-                </div>
+    try {
+        const resp = await fetch(`${BACKEND_BASE}/news`, { method: "GET", cache: "no-store" })
+        if (resp.status !== 200) {
+            throw new Error()
+        } else {
+            const entries = await resp.json()
+            entries.map(entry => {
+                const html_to_insert = DOMPurify.sanitize(
+                    entry.content
+                        .replaceAll("<h1>", `<h1 class=${styles.BigHeading}>`)  // apply h1 formatting
+                        .replaceAll("<h2>", `<h1 class=${styles.SmallHeading}>`) // apply h2 formatting
+                        .replaceAll("<h3>", `<h1 class=${styles.VerySmallHeading}>`) //apply h3 formatting
+                        .replaceAll("<div>", `<div class=${styles.Textblock}>`) // apply Textbox formatting to divs
+                )
+                blog_entries.push(
+                    <div className={localStyle.Blogpost} key={entry.id}>
+                        <h4>{
+                            // reformat date string
+                            entry.date
+                                .split("-")
+                                .reverse()
+                                .join(".")
+                        }</h4>
+                        <div dangerouslySetInnerHTML={{ __html: html_to_insert }} />
+                    </div>
+                )
+            }
             )
         }
+    } catch {
+        blog_entries.push(
+            <>
+                <h1 className={styles.BigHeading}>News</h1>
+                <FetchError />
+            </>
         )
     }
 
