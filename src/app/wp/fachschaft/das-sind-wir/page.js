ecafd1f2bb3c66fd53bd91dba40393f0ff7a40c6
= <jonhur@web.de>
2024-11-03 23:52:06 +0100

    outsourced to external api

--- src/app/wp/fachschaft/das-sind-wir/page.js
+++ src/app/wp/fachschaft/das-sind-wir/page.js
@@ -3,11 +3,23 @@ import styles from "@/app/page.module.css"
 import Image from "next/image"
 import Fachschaft from "@/img/fachschaft.jpg"
 
-import rollen from "@/../content/gi/page-content/fachschaft/rollen.json"
+import error_data from "./rollen_error_data.json"
+import { BACKEND_BASE } from "@/app/BACKEND_URL"
 
-export default function Content() {
+export default async function Content() {
 
-    // rollen variable is importet on top
+    var rollen = {}
+    try {
+        const resp = await fetch(`${BACKEND_BASE}/fachschaft_rollen`, { cache: "no-store" })
+        if (resp.status != 200) {
+            throw new Error();
+        } else {
+            var rollen = await resp.json()
+        }
+    } catch {
+        var rollen = { ...error_data }
+        console.log(error_data)
+    }
 
     return (
         <>
@@ -21,7 +33,7 @@ export default function Content() {
                 Am {rollen.wahldatum} ist der aktuelle Fachschaftsrat gewählt worden. Das Protokoll
                 gibt’s <a href={rollen.protokoll}>hier</a>.
             </h3>
-            {rollen.roles.map(role => <Role role={role.role} names={role.names} description={role.description} key={role}/>)}
+            {rollen.roles.map(role => <Role role={role.role} names={role.names} description={role.description} key={role} />)}
         </>
     )
 }
