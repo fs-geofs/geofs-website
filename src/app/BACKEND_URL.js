5682e4c9706a910310e00b03eeccc7e4cf098e1a
= <jonhur@web.de>
2024-11-09 21:12:52 +0100

    removed protokoll url

--- src/app/BACKEND_URL.js
+++ src/app/BACKEND_URL.js
@@ -1,2 +1,1 @@
-export const BACKEND_BASE = "http://" + process.env.BACKEND_URL + ":" + process.env.BACKEND_PORT
-export const PROTOKOLL_URL = process.env.PROTOKOLL_ENDPOINT;
+export const BACKEND_BASE = "http://" + process.env.BACKEND_URL + ":" + process.env.BACKEND_PORT;
