d60a62bc8ff54c4ad48fa0c203d1f4c3df42e1c7
= <jonhur@web.de>
2024-11-09 20:39:37 +0100

    added sitzungsprotokolle to site

--- src/app/(gi)/wp/fachschaft/protokolle/page.js
+++ src/app/(gi)/wp/fachschaft/protokolle/page.js
@@ -1,14 +1,17 @@
 import styles from "@/app/page.module.css"
 
+import { PROTOKOLL_URL } from "@/app/BACKEND_URL"
+
 export default function Content() {
+    console.log(PROTOKOLL_URL)
     return (
         <>
-            <h1 className={styles.BigHeading}>Protokolle</h1>
-            <div className={styles.Textblock}>
-                Die Protokolle zu den wöchentlichen Sitzungen findest du hier:
+            <h1 className={styles.BigHeading}>Sitzungsprotokolle</h1>
+            <div style={{border: "1px solid lightgrey", width: "100%", padding: "5px"}}>
+                <iframe src={PROTOKOLL_URL} width="100%" height="500px" allowFullScreen></iframe>
             </div>
-            <div className={styles.Textblock} style={{border: "3px solid red", padding: "5px"}}>
-                <strong>Under Construction!</strong>
+            <div className={styles.Textblock}>
+                oder <a href={PROTOKOLL_URL} target="_blank">hier</a>.
             </div>
         </>
     )
--- src/app/BACKEND_URL.js
+++ src/app/BACKEND_URL.js
@@ -1,1 +1,2 @@
 export const BACKEND_BASE = "http://" + process.env.BACKEND_URL + ":" + process.env.BACKEND_PORT
+export const PROTOKOLL_URL = process.env.PROTOKOLL_ENDPOINT;
