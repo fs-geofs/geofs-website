166e7d9fe343ba68851974377a52246d1cb3aced
= <jonhur@web.de>
2024-11-09 21:16:45 +0100

    force dynamic rendering

--- src/app/(gi)/wp/fachschaft/protokolle/page.js
+++ src/app/(gi)/wp/fachschaft/protokolle/page.js
@@ -3,16 +3,20 @@ import styles from "@/app/page.module.css"
 import { PROTOKOLL_URL } from "@/app/BACKEND_URL"
 
 export default function Content() {
-    console.log(PROTOKOLL_URL)
+    
+    const protokoll_url = process.env.PROTOKOLL_ENDPOINT
+
     return (
         <>
             <h1 className={styles.BigHeading}>Sitzungsprotokolle</h1>
             <div style={{border: "1px solid lightgrey", width: "100%", padding: "5px"}}>
-                <iframe src={PROTOKOLL_URL} width="100%" height="500px" allowFullScreen></iframe>
+                <iframe src={protokoll_url} width="100%" height="500px" allowFullScreen></iframe>
             </div>
             <div className={styles.Textblock}>
-                oder <a href={PROTOKOLL_URL} target="_blank">hier</a>.
+                oder <a href={protokoll_url} target="_blank">hier</a>.
             </div>
         </>
     )
 }
+
+export const dynamic = "force-dynamic";
