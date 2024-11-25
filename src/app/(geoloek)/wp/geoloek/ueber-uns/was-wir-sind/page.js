c5cd752df3a64b57486fafb2ac1dc631126111b7
= <jonhur@web.de>
2024-11-25 19:02:16 +0100

    added boilerplate

---
+++ src/app/(geoloek)/wp/geoloek/ueber-uns/was-wir-sind/page.js
@@ -0,0 +1,12 @@
+import styles from "@/app/page.module.css";
+
+export default function Page() {
+    return(
+        <>
+            <h1 className={styles.BigHeading}>Wer wir sind</h1>
+            <div className={styles.Textblock}>
+                Diese Seite befindet sich aktuell noch im Aufbau.
+            </div>
+        </>
+    )
+}
