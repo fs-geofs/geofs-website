cfe53c9b456c81c4f555767ff096e1d2adf5ee0b
= <jonhur@web.de>
2024-12-22 11:12:06 +0100

    added content

--- src/app/(geoloek)/wp/geoloek/ueber-uns/aemter/page.js
+++ src/app/(geoloek)/wp/geoloek/ueber-uns/aemter/page.js
@@ -1,11 +1,21 @@
 import styles from "@/app/page.module.css";
 
 export default function Page() {
-    return(
+    return (
         <>
-            <h1 className={styles.BigHeading}>Ämter</h1>
+            <h1 className={styles.BigHeading}>Ämter und Gremien</h1>
             <div className={styles.Textblock}>
-                Diese Seite befindet sich aktuell noch im Aufbau.
+                Wir, das heißt die gewählten Mitglieder des Fachschaftsrates
+                Geographie/Landschaftsökologie, sind für euch als studentische
+                Vertreter in diversen Gremien vertreten. Wer mehr zum Thema
+                Organisation und Legitimation der „verfassten Studierendenschaft“
+                wissen möchte, findet <a href="http://www.stupa.ms/studierendenschaft/">hier</a> nähere
+                Informationen.
+            </div>
+            <div>
+                Wer euch in welchen Gremien vertritt, könnt ihr im Folgenden nachlesen.
+                Da die Gremien fachbereichsweit besetzt werden, sind auch Mitglieder der
+                Fachschaftsräte Geoinformatik (GI) und Geowissenschaften (Gw) in den Gremien vertreten.
             </div>
         </>
     )
