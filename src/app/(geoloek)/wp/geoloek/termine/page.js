42c888c5c258cb1e278fc8392592d4b8d800ad62
= <jonhur@web.de>
2024-12-22 10:16:21 +0100

    added termine page

--- src/app/(geoloek)/wp/geoloek/termine/page.js
+++ src/app/(geoloek)/wp/geoloek/termine/page.js
@@ -1,12 +1,78 @@
 import styles from "@/app/page.module.css";
 
 export default function Page() {
-    return(
+
+    const termine = {
+        stand: "01.01.1901",
+        fstermine: [
+            {
+                date: "01.01.1901",
+                description: "lalala",
+                infolink: null
+            },
+            {
+                date: "01.01.1901",
+                description: "lolol",
+                infolink: "https://google.com"
+            }
+        ],
+        other: [
+            {
+                date: "Donenrstags 10-14 Uhr",
+                description: "Donnerstags 10-14 Uhr",
+                infolink: "https://www.uni-muenster.de/Nachhaltigkeit/brotzeitkolloquium.html"
+            },
+            {
+                date: "Jeden letzten Freitag im Monat",
+                description: "Offene Gesprächsrunde zum Thema \"Studieren mit Autismus\""
+            }
+        ]
+    }
+
+    return (
         <>
-            <h1 className={styles.BigHeading}>Termine</h1>
+            <h1 className={styles.BigHeading}>Termine Termine Termine</h1>
             <div className={styles.Textblock}>
-                Diese Seite befindet sich aktuell noch im Aufbau.
+                Wann war nochmal die nächste Geoparty?  Alle wichtigen Termine rund ums Studium und
+                sonstigen Veranstaltungen der Fachschaft findet ihr hier.
+            </div>
+            <div className={styles.Textblock}>
+                Termine der Facschaft und des Fachbereichs:
+            </div>
+            <div className={styles.Textblock}>
+                <Terminliste terminliste={termine.fstermine} />
+            </div>
+            <div className={styles.Textblock}>
+                Das könnte für euch auch interessant sein:
+            </div>
+            <div className={styles.Textblock}>
+                <Terminliste terminliste={termine.other} />
+            </div>
+            <div className={styles.Textblock}>
+                <i>(Stand: {termine.stand})</i>
             </div>
         </>
     )
+}
+
+const Terminliste = ({ terminliste }) => {
+    return (
+        <ul>
+            {terminliste.map(
+                termin => <Termin key={termin.date+termin.description} date={termin.date} description={termin.description} infolink={termin.infolink}/>
+            )}
+        </ul>
+    )
+}
+
+const Termin = ({ date, description, infolink }) => {
+    return (
+        <li>
+            <strong>{date}</strong>: {description} {infolink ? <Infolink infolink={infolink} /> : ""}
+        </li>
+    )
+}
+
+const Infolink = ({ infolink }) => {
+    return <a href="infolink">Info</a>
 }
