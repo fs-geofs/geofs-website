503dca299ab05b15a67cca05922730647e30a7b8
= <jonhur@web.de>
2024-12-22 11:06:26 +0100

    added page content

--- src/app/(geoloek)/wp/geoloek/erstis/erstifahrt/page.js
+++ src/app/(geoloek)/wp/geoloek/erstis/erstifahrt/page.js
@@ -1,11 +1,59 @@
 import styles from "@/app/page.module.css";
 
 export default function Page() {
-    return(
+    return (
         <>
             <h1 className={styles.BigHeading}>Ersti-Fahrt</h1>
             <div className={styles.Textblock}>
-                Diese Seite befindet sich aktuell noch im Aufbau.
+                Die Ersti-Fahrt XXXX findet am Wochenende vom XX.XX.XXXX bis XX.XX.XXXX statt.
+                Wie (fast) immer geht es nach <a href="http://www.druebberholz.de/">Drübberholz</a> (Dörverden).
+                Mitfahren können insgesamt 70 Erstis der Studiengänge Landschaftsökologie, Geographie und Geoinformatik.
+            </div>
+            <div className={styles.Textblock}>
+                Geboten wird eine Menge: Wir rennen im Wald rum, feiern, lernen Mitstudenten kennen und lieben, feiern,
+                lernen nützliche Dinge über Pflanzen, Tiere, Wind, Wetter und GPS-Geräte, feiern und haben viel
+                Spaß dabei. Wer nicht mitfährt, ist selber Schuld, denn es wird jede Menge gefeiert!
+            </div>
+            <div className={styles.Textblock}>
+                Der Unkostenbeitrag beträgt (voraussichtlich) XX€ (inkl. Fahrtkosten, Verpflegung, Unterbringung in Mehrbettzimmern)
+                und ist in bar und möglichst passend bei der Anmeldung während des Geobrunches zu zahlen. Dies ist die einzige
+                Anmeldegelegenheit! Wer zu diesem Zeitpunkt verhindert ist, kann sich leider nicht von einer anderen Person vertreten
+                lassen. Die Anmeldung ist verbindlich. Wenn eine angemeldete Person an der Fahrt nicht mehr teilnehmen kann/will,
+                ist die Rückerstattung des Teilnehmerbetrages nur möglich, wenn selbständig ein Nachfolger gestellt wird
+                (je nach Andrang auf der Warteliste).
+            </div>
+            <h2 className={styles.SmallHeading}>Mitbringen</h2>
+            <div className={styles.Textblock}>
+                <ul>
+                    <li>Ausreichend Proviant für Freitagmittag und Nachmittag (lange Busfahrt)</li>
+                    <li>Brotdose und Trinkflasche für Lunchpakete</li>
+                    <li>einen sehr persönlichen Gegenstand!</li>
+                    <li>ein Gesellschaftsspiel</li>
+                    <li>Musik: CDs, Gitarre oder andere Instrumente, Texte</li>
+                    <li>Hausschuhe (nicht brennbar!)</li>
+                    <li>Kleingeld (für Bier, Cola, Saft, Wasser,…)</li>
+                    <li>Bettzeug/Schlafsack</li>
+                    <li>Handtücher</li>
+                    <li>Regenkleidung (warm & trocken)</li>
+                    <li>Schreibzeug, Klemmbrett (wer hat),…</li>
+                    <li>Gute Laune!</li>
+                    <li>Wanderschuhe</li>
+                    <li>2tes-ntes Paar Schuhe</li>
+                    <li>Deo und Duschgel (im Interesse eurer Mitmenschen)</li>
+                </ul>
+            </div>
+            <div className={styles.Textblock}>
+                Zu dokumentarischen Zwecken, für eventuelle Sozialstudien und wegen der Sicherheit
+                wird die Ersti-Fahrt in Ton und bewegtem Bild für die Nachwelt festgehalten. Wer das
+                lieber vermeiden möchte, sollte aus organisatorischen Gründen im Voraus(!) Rücksprache
+                mit der Fachschaft halten.
+            </div>
+            <div className={styles.Textblock}>
+                Aus juristischen Gründen ist eine Teilnahme erst ab 18 Jahren möglich, Ausweise werden
+                bei der Anmeldung kontrolliert!!!
+            </div>
+            <div className={styles.Textblock}>
+                Es gibt auch einen kleinen Absatz in der FAQ zum Ersti-WE: <a href="/wp/geoloek/erstis/ersti-fragen/">Ersti-FAQ</a>
             </div>
         </>
     )
