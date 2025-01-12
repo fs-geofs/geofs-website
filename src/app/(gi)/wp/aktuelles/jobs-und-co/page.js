e0af6c0c4c46d42adaf5e2ce8795797f142496ec
= <jonhur@web.de>
2025-01-12 16:54:00 +0100

    refactored for reusability

--- src/app/(gi)/wp/aktuelles/jobs-und-co/page.js
+++ src/app/(gi)/wp/aktuelles/jobs-und-co/page.js
@@ -1,52 +1,7 @@
-import styles from "@/app/page.module.css"
-import localStyle from "./page.module.css"
-
 import { BACKEND_BASE } from "@/app/BACKEND_URL"
-import FetchError from "@/app/components/error_fetching"
-
-import DOMPurify from "isomorphic-dompurify"
+import Jobpage from "@/app/components/jobspage"
 
 export default async function Site() {
-
-    const job_entries = []
-
-    // call API to fetch news articles
-    try {
-        const resp = await fetch(`${BACKEND_BASE}/joblistings`, { method: "GET", cache: "no-store" })
-        if (resp.status !== 200) {
-            throw new Error()
-        } else {
-            const entries = await resp.json()
-            entries.map(entry => {
-                const html_to_insert = DOMPurify.sanitize(
-                    entry.content
-                        .replaceAll("<h1>", `<h1 class=${styles.BigHeading}>`)  // apply h1 formatting
-                        .replaceAll("<h2>", `<h1 class=${styles.SmallHeading}>`) // apply h2 formatting
-                        .replaceAll("<h3>", `<h1 class=${styles.VerySmallHeading}>`) //apply h3 formatting
-                        .replaceAll("<div>", `<div class=${styles.Textblock}>`) // apply Textbox formatting to divs
-                )
-                job_entries.push(
-                    <div className={localStyle.Blogpost} key={entry.id}>
-                        <div dangerouslySetInnerHTML={{ __html: html_to_insert }} />
-                    </div>
-                )
-            })
-        }
-    } catch {
-        return (
-            <>
-            <h1 className={styles.BigHeading}>Jobs und Co</h1>
-            <FetchError />
-        </>
-        )
-    }
-
-
-
-    return (
-        <>
-            <h1 className={styles.BigHeading}>Jobs und Co</h1>
-            {job_entries.map(entry => entry)}
-        </>
-    )
+    const jobs_endpiont_url = `${BACKEND_BASE}/joblistings`
+    return <Jobpage backend_jobs_endpoint={jobs_endpiont_url} />
 }
