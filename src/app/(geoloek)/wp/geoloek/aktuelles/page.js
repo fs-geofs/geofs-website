5a6afe681d05c37ecafe9b426cc5b6f994b288f1
= <jonhur@web.de>
2025-01-12 18:55:24 +0100

    added redirects

---
+++ src/app/(geoloek)/wp/geoloek/aktuelles/page.js
@@ -0,0 +1,11 @@
+import {redirect} from 'next/navigation'
+
+/*
+Diese Seite hat keine Content und soll nicht verlinkt werden.
+Autamatisches Reirect zur ersten Child-Seite (aus der Navbar)
+falls irgendein findiger Nutzer diese URL ansteuert.
+*/
+
+export default function Content () {
+    redirect("/wp/geoloek/aktuelles/news")
+}
