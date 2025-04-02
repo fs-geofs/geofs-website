8cc84372b76f892ab0eb45f61592c2b5c990db28
= <jonhur@web.de>
2025-04-02 20:22:48 +0200

    auto-upgrade to next15

--- src/app/(gi)/wp/aktuelles/news/page.js
+++ src/app/(gi)/wp/aktuelles/news/page.js
@@ -2,7 +2,8 @@ import Newspage from "@/app/components/newspage"
 
 import { BACKEND_BASE } from "@/app/BACKEND_URL"
 
-export default async function Site({ searchParams }) {
+export default async function Site(props) {
+    const searchParams = await props.searchParams;
 
     const endpoint_url = `${BACKEND_BASE}/news`
     return <Newspage searchParams={searchParams} newsEndpoint={endpoint_url}/>
